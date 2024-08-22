5a832145352a6692f4a64ad00be496c3e890cdd1
NicholasHickmanKeuda <nicholas.hickman@edu.keuda.fi>
2024-08-22 09:59:15 +0300

    8

---
+++ Harjoitus 8/Harjoitus 8/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Harjoitus_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            TekstiTB = new TextBox();
+            VastausLB = new Label();
+            MuutaBT = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Snap ITC", 48F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(23, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(809, 82);
+            label1.TabIndex = 0;
+            label1.Text = "Roomalaiset Numerot";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(41, 115);
+            label2.Name = "label2";
+            label2.Size = new Size(523, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Anna numero välillä 1 - 3999, niin muutan sen roomalaiseksi:";
+            // 
+            // TekstiTB
+            // 
+            TekstiTB.Location = new Point(598, 117);
+            TekstiTB.Name = "TekstiTB";
+            TekstiTB.Size = new Size(100, 23);
+            TekstiTB.TabIndex = 2;
+            // 
+            // VastausLB
+            // 
+            VastausLB.AutoSize = true;
+            VastausLB.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            VastausLB.Location = new Point(41, 203);
+            VastausLB.Name = "VastausLB";
+            VastausLB.Size = new Size(73, 50);
+            VastausLB.TabIndex = 3;
+            VastausLB.Text = "xxx";
+            VastausLB.Visible = false;
+            // 
+            // MuutaBT
+            // 
+            MuutaBT.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            MuutaBT.Location = new Point(598, 220);
+            MuutaBT.Name = "MuutaBT";
+            MuutaBT.Size = new Size(100, 33);
+            MuutaBT.TabIndex = 4;
+            MuutaBT.Text = "Muuta";
+            MuutaBT.UseVisualStyleBackColor = true;
+            MuutaBT.Click += MuutaBT_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(844, 305);
+            Controls.Add(MuutaBT);
+            Controls.Add(VastausLB);
+            Controls.Add(TekstiTB);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Numeroiden muutis latinalaisista roomalaisiksi";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox TekstiTB;
+        private Label VastausLB;
+        private Button MuutaBT;
+    }
+}
